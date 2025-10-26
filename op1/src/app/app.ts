dc39d09cc90d2522a8786ec4138233fedfdef2fb
AxelBlois <axel.blois@edu.esiee.fr>
2025-10-26 22:57:14 +0100

    op1 acompo + model

--- op1/src/app/app.ts
+++ op1/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FaceSnap} from './face-snap/face-snap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FaceSnap],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('op1');
-}
+export class App {}
