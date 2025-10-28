f1a5f30d5c33877488dbb504454c0466f61f7661
AxelBlois <axel.blois@edu.esiee.fr>
2025-10-28 19:49:29 +0100

    Début observable angular OP2

--- op1/src/app/app.ts
+++ op1/src/app/app.ts
@@ -1,23 +1,43 @@
-import {Component, OnInit, signal} from '@angular/core';
-import {FaceSnap} from './models/face-snap';
-import { HeaderComponent} from './header/header';
-import {RouterOutlet} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {concatMap, delay, exhaustMap, interval, map, mergeMap, of, switchMap, take, tap} from 'rxjs';
 
 
 @Component({
   selector: 'app-root',
-  imports: [
-    HeaderComponent,
-    RouterOutlet,
-  ],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
-  faceSnaps!: FaceSnap[];
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      switchMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
   }
 
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({color, trainIndex}).pipe(
+      delay(isRedTrain ? 5000 : 6000)
+    );
+
+  }
+
+
+  translateColor(color: 'rouge' | 'jaune') {
+
+    return color === 'rouge' ? 'red' : 'yellow';
+
+  }
 
 }
